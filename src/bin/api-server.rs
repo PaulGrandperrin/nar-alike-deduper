aecc7260dd51b792a5043c25ad4abf3f517f7d08
Paul Grandperrin <paul.grandperrin@gmail.com>
2024-03-18 15:48:42 +0100

    WIP download and dedup

--- src/bin/api-server.rs
+++ src/bin/api-server.rs
@@ -11,18 +11,8 @@ use serde::{Deserialize, Serialize};
 async fn main() -> eyre::Result<()> {
   nar_alike_deduper::setup_logging()?;
 
-  //let mut decompress_from_stdin = async_compression::tokio::bufread::XzDecoder::new(tokio::io::BufReader::new(stdin()));
-  //let mut compress_to_stdout = async_compression::tokio::write::XzEncoder::with_quality(stdout(), Level::Precise(9)); //6
   
   
-  //let reader = File::open("file.nar").await?;
-  //let mut writer = AsyncSha256Hasher::new();
-  //replace_nix_paths(decompress_from_stdin, &mut compress_to_stdout).await?;
-  //let hash = writer.finalize();
-  //println!("{}", hex::encode(hash));
-  
-  //tokio::io::copy(&mut stdin(), &mut compress_to_stdout).await?;
-  //compress_to_stdout.shutdown().await?;
   
   //http_server(()).await?;
 
--- src/bin/download-and-compute-dedup-hashes.rs
+++ src/bin/download-and-compute-dedup-hashes.rs
@@ -1,8 +1,72 @@
 use async_channel::Receiver;
 use color_eyre::eyre;
+use nar_alike_deduper::AsyncSha256Hasher;
 use sqlx::{postgres::{PgPoolOptions, PgTypeInfo, types::Oid}, Row, Column};
 use futures::{TryStreamExt, StreamExt};
+use tokio_util::io::StreamReader;
 use std::{fmt::Write, collections::HashMap, sync::{atomic::{AtomicU64, Ordering}, Arc}};
+          
+async fn process_hash2(client: &reqwest::Client, hash: String, total: &Arc<AtomicU64>, i: &Arc<AtomicU64>) -> eyre::Result<()> {
+  let r = client.get(&format!("http://cache.nixos.org/{}.narinfo", hash)).send().await?;
+  if r.status() != 200 {
+    return Err(eyre::eyre!("bad status: {}", r.status()));
+  }
+  let r = r.text().await?; // maybe https is faster
+  let d = r.lines().filter_map(|l| {
+    l.find(": ").map(|i| (&l[..i], &l[i+2..]))
+  }).collect::<HashMap<_,_>>();
+  //dbg!(&d);
+  let size: u64 = d.get("FileSize").ok_or(eyre::eyre!("no file size"))?.parse()?;
+  total.fetch_add(size, Ordering::SeqCst);
+  let total = total.load(Ordering::SeqCst);
+  let i = i.load(Ordering::SeqCst);
+  println!("{}: {} / {} = {}", hash, total, i, total / i);
+  Ok(())
+}
+
+async fn process_hash(client: &reqwest::Client, hash: String, total: &Arc<AtomicU64>, i: &Arc<AtomicU64>) -> eyre::Result<()> {
+  println!("processing {}", hash);
+  let r = client.get(&format!("http://cache.nixos.org/{}.narinfo", hash)).send().await?;
+  if r.status() != 200 {
+    return Err(eyre::eyre!("bad status: {}", r.status()));
+  }
+  let r = r.text().await?; // maybe https is faster
+  println!("{}", r);
+  let d = r.lines().filter_map(|l| {
+    l.find(": ").map(|i| (&l[..i], &l[i+2..]))
+  }).collect::<HashMap<_,_>>();
+  //dbg!(&d);
+  let size: u64 = d.get("FileSize").ok_or(eyre::eyre!("no file size"))?.parse()?;
+  total.fetch_add(size, Ordering::SeqCst);
+  let total = total.load(Ordering::SeqCst);
+  let i = i.load(Ordering::SeqCst);
+  println!("{}: {} / {} = {}", hash, total, i, total / i);
+
+  let url = d.get("URL").ok_or(eyre::eyre!("no url"))?;
+  let r = client.get(format!("http://cache.nixos.org/{}", url)).send().await?;
+  if r.status() != 200 {
+    return Err(eyre::eyre!("bad status: {}", r.status()));
+  }
+
+  // convert a Steamer of Bytes to an AsyncReader
+  let bs = r.bytes_stream();
+  let ms = bs.map(|result| result.map_err(|err| {
+    std::io::Error::new(std::io::ErrorKind::Other, err)
+  }));
+  let sr = StreamReader::new(ms);
+
+
+  let mut hasher = AsyncSha256Hasher::new();
+  tokio::io::copy(
+    &mut async_compression::tokio::bufread::XzDecoder::new(sr),
+    &mut hasher
+  ).await?;
+  let narhash = hasher.finalize();
+  println!("computed hash: {}", hex::encode(narhash));
+  println!("computed hash in base32: {}", nix_base32::to_nix_base32(&narhash));
+
+  Ok(())
+}
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -16,11 +80,11 @@ async fn main() -> eyre::Result<()> {
   tracing::info!("connected to db");
   
   // setting up workers
-  let mut total = Arc::new(AtomicU64::new(0));
-  let mut i = Arc::new(AtomicU64::new(0));
+  let total = Arc::new(AtomicU64::new(0));
+  let i = Arc::new(AtomicU64::new(0));
 
   let (send, recv) = async_channel::bounded(1000);
-  let tasks = (0..16).map(|thread_id| {
+  let tasks = (0..1).map(|thread_id| {
     tracing::info!(thread_id, "starting insertion");
 
     let recv = recv.clone();
@@ -29,28 +93,12 @@ async fn main() -> eyre::Result<()> {
     let i = i.clone();
 
     tokio::task::spawn(async move {
-      let c = reqwest::Client::new();
+      let client = reqwest::Client::new();
 
       while let Ok(hash) = recv.recv().await {
         i.fetch_add(1, Ordering::SeqCst);
-        let r: eyre::Result<()> = async {
-          return Ok(());
-          let r = c.get(&format!("http://cache.nixos.org/{}.narinfo", hash)).send().await?;
-          if r.status() != 200 {
-            return Err(eyre::eyre!("bad status: {}", r.status()));
-          }
-          let r = r.text().await?; // maybe https is faster
-          let d = r.lines().filter_map(|l| {
-            l.find(": ").map(|i| (&l[..i], &l[i+2..]))
-          }).collect::<HashMap<_,_>>();
-          //dbg!(&d);
-          let size: u64 = d.get("FileSize").ok_or(eyre::eyre!("no file size"))?.parse()?;
-          total.fetch_add(size, Ordering::SeqCst);
-          let total = total.load(Ordering::SeqCst);
-          let i = i.load(Ordering::SeqCst);
-          println!("{}: {} / {} = {}", hash, total, i, total / i);
-          Ok(())
-        }.await;
+
+        let r = process_hash(&client, hash, &total, &i).await;
 
         if let Err(e) = r {
           tracing::error!(thread_id, ?e);
@@ -83,6 +131,8 @@ async fn main() -> eyre::Result<()> {
     if let Err(e) = r {
       tracing::error!(?e);
     } 
+
+    break;
 
   }
   drop(send);
